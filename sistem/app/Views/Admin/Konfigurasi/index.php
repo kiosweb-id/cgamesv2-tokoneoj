58aec87207ff593acc59d262256a25cf2ff3c5b2
Kiosweb <122413524+kioswebid@users.noreply.github.com>
2023-01-21 02:01:19 +0000

    delete form social

--- sistem/app/Views/Admin/Konfigurasi/index.php
+++ sistem/app/Views/Admin/Konfigurasi/index.php
@@ -78,10 +78,11 @@
 								</div>
 							</div>
 						</div>
-
-						<div class="row">
+						 
+						<!--
+							<div class="row">
 								<div class="col-lg-12 mx-auto">
-								<!-- Social Media -->
+								<!-- Social Media 
 								<div class="card shadow mb-4">
 								<div class="card-header py-3">
 								<h6 class="m-0 font-weight-bold text-primary">Social Media</h6>
@@ -127,6 +128,7 @@
 							</div>
 							</div>
 						</div>
+						--> 
 
 						<div class="row">
 							<div class="col-lg-12 mx-auto">
